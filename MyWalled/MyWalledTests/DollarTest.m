271e0b607151c0523b5772179f970044702105bf
Alicia Daza <amdazacastillo@gmail.com>
2016-07-20 21:17:06 +0200

    Dollar multiplication test

---
+++ MyWalled/MyWalledTests/DollarTest.m
@@ -0,0 +1,35 @@
+//
+//  DollarTest.m
+//  MyWalled
+//
+//  Created by Alicia Daza on 20/07/16.
+//  Copyright © 2016 Alicia Daza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DollarTest : XCTestCase
+
+@end
+
+@implementation DollarTest
+
+- (void)testSimpleMultiplication {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    Dollar *five = [[Dollar alloc] initWithAmount: 5];
+    Dollar *product = [five times:2];
+    Dollar *shouldResult = [[Dollar alloc] initWithAmount: 10];
+    
+    XCTAssertEqualObjects(shouldResult, product,
+                          @"€5 times 2 should be €10");
+    
+    product = [five times: 3];
+    shouldResult = [[Dollar alloc] initWithAmount: 15];
+    
+    XCTAssertEqualObjects(product, shouldResult,
+                          @"€5 times 3 should be €15");
+}
+
+@end
