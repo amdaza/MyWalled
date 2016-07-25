7b92d22f4d9edde3fb44419a3ce1a930ce0a1abd
Alicia Daza <amdazacastillo@gmail.com>
2016-07-25 16:38:32 +0200

    Test simple reduction for broker. Not yet implemented

---
+++ MyWalled/MyWalledTests/BrokerTests.m
@@ -0,0 +1,40 @@
+//
+//  BrokerTests.m
+//  MyWalled
+//
+//  Created by Alicia Daza on 25/07/16.
+//  Copyright © 2016 Alicia Daza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Money.h"
+
+@interface BrokerTests : XCTestCase
+
+@end
+
+@implementation BrokerTests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+-(void) testSimpleReduction {
+    Broker *broker = [[Broker alloc] init];
+    Money *sum = [[Money dollarWithAmount:5] plus:[Money dollarWithAmount:5]];
+    
+    Money *reduced = [broker reduce: sum toCurrency: @"USD"];
+    
+    XCTAssertEqualObjects(sum, reduced, @"Conversion to same currency should be a NOP");
+}
+
+@end
