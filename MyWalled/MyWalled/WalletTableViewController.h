c7861f90d5e1b4e86c79ee2ff2e58feea916354d
Alicia Daza <amdazacastillo@gmail.com>
2016-07-28 22:11:50 +0200

    Created tests for wallet table

---
+++ MyWalled/MyWalled/WalletTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WalletTableViewController.h
+//  MyWalled
+//
+//  Created by Alicia Daza on 28/07/16.
+//  Copyright © 2016 Alicia Daza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WalletTableViewController : UITableViewController
+
+@end
--- MyWalled/MyWalledTests/ControllerTests.m
+++ MyWalled/MyWalledTests/ControllerTests.m
@@ -8,12 +8,17 @@
 
 #import <XCTest/XCTest.h>
 #import "SimpleViewController.h"
+#import "WalletTableViewController.h"
+#import "Wallet.h"
 
 @interface ControllerTests : XCTestCase
 
 @property (nonatomic, strong) SimpleViewController *simpleVC;
 @property (nonatomic, strong) UIButton *button;
 @property (nonatomic, strong) UILabel *label;
+
+@property (nonatomic, strong) WalletTableViewController *walletTVC;
+@property (nonatomic, strong) Wallet *wallet;
 
 @end
 
@@ -23,11 +28,19 @@
     [super setUp];
     
     // Create laboratory environment
+    
+    // Button & label
     self.simpleVC = [[SimpleViewController alloc] initWithNibName:nil bundle:nil];
     self.button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [self.button setTitle:@"Test" forState:UIControlStateNormal];
     self.label = [[UILabel alloc] initWithFrame:CGRectZero];
     self.simpleVC.displayLabel = self.label;
+
+    // Wallet table
+    self.wallet = [[Wallet alloc] initWithAmount:1
+                                        currency:@"USD"];
+    [self.wallet plus: [Money euroWithAmount:1]];
+    self.walletTVC = [[WalletTableViewController alloc] initWithModel: wallet];
 }
 
 - (void)tearDown {
@@ -36,12 +49,26 @@
     [super tearDown];
 }
 
+
+// Button & label text test
 - (void)testTextOnLabelOnPushedButton{
     // Send message
     [self.simpleVC displayText: self.button];
     
     // Check that label and button have the same text
     XCTAssertEqualObjects(self.button.titleLabel.text, self.label.text, @"Button and label should have the same text");
+}
+
+
+-(void) testTableHasOneSection {
+    
+    NSUInteger sections = [self.walletTVC numberOfSectionsInTableView:nil];
+    XCTAssertEqual(sections, 1,
+                   @"Only one section");
+}
+
+-(void) testNumberOfCells {
+    
 }
 
 @end
